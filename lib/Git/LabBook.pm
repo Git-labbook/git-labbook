5f3a0ad0559ff24d6b28a8332eaf374d55f1a44e
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2016-06-02 20:55:04 +0200

    Add new options and improve existing commands

--- lib/Git/LabBook.pm
+++ lib/Git/LabBook.pm
@@ -8,6 +8,11 @@ use Git::LabBook::Types;
 use Git::LabBook::ConfigAttribute;
 use Path::Class;
 
+has_config_file 'labbookstate' => (
+    optnames => [ ],
+    default => file('.labbook-state'),
+    );
+
 has_config_file 'labbookfile' => (
     optnames => [ 'labbook-file-name' ],
     default => file('LabBook.org'),
@@ -33,6 +38,16 @@ has_config_str 'databranch' => (
     default => 'data',
     );
 
+has_config_str 'xpbranch' => (
+    optnames => [ 'xp-branch-name' ],
+    default => 'xp/%s',
+    );
+
+has_config_bool 'labbookentry' => (
+    optnames => [ 'labbook-entry' ],
+    default => 1,
+    );
+
 sub set {
     my $self = shift;
     my $optname = shift;
@@ -45,9 +60,21 @@ sub set {
 	die "Invalid option name $optname";
     }
     $realname = '_set_'.$realname;
-
     $self->$realname($value);
 }
+
+around 'xpbranch' => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    die "Missing experiment name"
+	unless @_;
+
+    my $xpname = shift;
+    my $branch=$self->$orig();
+    $branch =~ s/%s/$xpname/;
+    return $branch;
+};
 
 1;
 
